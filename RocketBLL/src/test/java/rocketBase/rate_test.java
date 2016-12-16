3479d9c8961319b10b7abd82b8817c6b70f7cc78
alexianebrao <alexianebrao@wifi-roaming-128-4-81-170.host.udel.edu>
2016-12-15 21:51:13 -0500

    Commit

---
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -0,0 +1,36 @@
+
+package rocketBase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import exceptions.RateException;
+
+public class rate_test {
+
+	
+	//		Check to see if a known credit score returns a known interest rate
+	
+	//		Check to see if a RateException is thrown if there are no rates for a given
+	//		credit score
+	@Test
+	public void test() {
+		assert(1==1);
+	}
+	
+	@Test
+	public void getratetest1() throws RateException {
+		assertEquals(RateBLL.getRate(700),4,1);
+	}
+	
+	@Test (expected = RateException.class)
+	public void getratetest2() throws RateException {
+		RateBLL.getRate(200);
+		
+		
+	}
+	
+
+}
---
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -0,0 +1,50 @@
+
+package rocketBase;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import rocketDomain.RateDomainModel;
+
+public class Rate_Test {
+
+	
+	//		Check to see if a known credit score returns a known interest rate
+	
+	//		Check to see if a RateException is thrown if there are no rates for a given
+	//		credit score
+	
+	ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+	
+	@Test
+	public void test() {
+		
+		
+		System.out.println ("Rates size: " + rates.size());
+		assert(rates.size() > 0);	
+	}
+	
+	
+		//	Check to see if a known credit score returns a known interest rate
+ 
+	@Test
+	public void test1() {
+		for(RateDomainModel rMOD : rates) {
+			System.out.println(rMOD.getdInterestRate());
+		}
+		
+		assertEquals(rates.get(0).getdInterestRate(),3.5,1);
+		assertEquals(rates.get(1).getdInterestRate(),3.75,1);
+		assertEquals(rates.get(2).getdInterestRate(),4.0,1);
+		assertEquals(rates.get(3).getdInterestRate(),4.5,1);
+		assertEquals(rates.get(4).getdInterestRate(),5.0,1);	
+		
+		
+		//		Check to see if a RateException is thrown if there are no rates for a given
+		//		credit score
+	}
+
+}
